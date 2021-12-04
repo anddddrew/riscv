d2f2b68da28eeb5427fe3f75cd2d138c90523b7c
Andrew <andrewnijmeh1@gmail.com>
2021-12-04 11:27:27 -0500

    add instructions

---
+++ src/instructions.rs
@@ -0,0 +1,67 @@
+use crate::formats::*;
+
+#[derive(Debug)]
+pub enum Instruction {
+    LUI(UType),
+    AUIPC(UType),
+
+    JAL(JType),
+    JALR(IType),
+
+    BEQ(BType).
+    BNE(BType),
+    BLT(BType),
+    BGE(BType),
+    BLTU(BType),
+    BGEU(BType),
+
+    LB(IType).
+    LH(IType),
+    LW(IType),
+    LBU(IType),
+    LHU(IType),
+
+    SB(SType),
+    SH(SType),
+    SW(SType),
+
+    ADDI(IType),
+    SLTI(IType),
+    SLTIU(IType),
+    XORI(IType),
+    ORI(IType),
+    ANDI(IType),
+    SLLT(IType),
+    SRLT(IType),
+    SRAI(IType),
+
+    ADD(RType),
+    SUB(RType),
+    SLL(RType),
+    SLT(RType),
+    SLTU(RType),
+    XOR(RType),
+    SRL(RType),
+    SRA(RType),
+    OR(RType),
+    AND(RType),
+
+    FENCE,
+
+    ECALL,
+    EBREAK,
+    
+    URET,
+    SRET,
+    MRET,
+    WFI,
+
+    // CSRI Instructions 
+    CSRRW,
+    CSRRS,
+    CSRRC,
+    CSRRWI,
+    CSRRSI,
+    CSRRCI,
+
+}
