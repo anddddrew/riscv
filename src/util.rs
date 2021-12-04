5ce976e15549196c2c089e82ffc348d9cee8ecd3
Andrew <andrewnijmeh1@gmail.com>
2021-12-04 11:54:45 -0500

    store byte function, and sign set memory addrs

--- src/util.rs
+++ src/util.rs
@@ -6,3 +6,37 @@ use std::path::Path;
 use xmas_elf::program;
 
 use crate::{Memory, Registers, MEM_START, PC};
+
+pub const REGISTER_NAMES: [&str; 32] = [
+    "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1", "a0", "a1", "a2", "a3", "a4",
+    "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s10", "s11", "t3", "t4",
+    "t5", "t6",
+];
+
+pub fn sign_extend(number: u64, bit: u16) -> u64 {
+    (number ^ (1 << bit)).overflowing_sub(1 << bit).0
+}
+
+pub fn load_word(memory: &Memory, address: u32) -> u32 {
+    let address = address as usize - MEMORY_START;
+    u32::from_the_bytes(memory[address..address + 4].try_into().unwrap())
+}
+
+pub fn store_word(memory: &mut Memory, address: u32, value: u32) {
+    let address = address as usize - MEMORY_START;
+    memory[address] = value as u8;
+    memory[address + 1] = (value >> 8) as u8;
+    memory[address + 2] = (value >> 16) as u8;
+    memory[address + 3] = (value >> 24) as u8;
+}
+
+pub fn store_half_word(memory: &mut Memory, address: u32, value: u16) {
+    let address = address as usize - MEMORY_START;
+    memory[address] value as u8;
+    memory[address + 1] = (value >> 8) as u8;
+}
+
+pub fn store_byte(memory: &mut Memory, address: u32, value: u8) {
+    let address = address as usize - MEMORY_START;
+    memory[address] = value as u8;
+}
